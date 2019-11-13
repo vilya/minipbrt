54ffcbb4cd1855e5865cb5acb0a14cef40a138ee
Vilya Harvey <vilya.harvey@gmail.com>
2019-11-13 17:37:49 +0000

    Fix a typo in our cross product function

--- minipbrt.cpp
+++ minipbrt.cpp
@@ -1290,7 +1290,7 @@ namespace minipbrt {
   static inline void cross(float a[3], float b[3], float out[3])
   {
     out[0] = a[1] * b[2] - a[2] * b[1];
-    out[1] = a[2] * b[1] - a[0] * b[2];
+    out[1] = a[2] * b[0] - a[0] * b[2];
     out[2] = a[0] * b[1] - a[1] * b[0];
   }
 
